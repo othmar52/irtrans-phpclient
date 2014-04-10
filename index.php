4156cd692ae2abacdac450855602d875ac789865
ann <ann@mmc-agentur.at>
2014-04-10 15:16:28 +0200

    clickdummy filesystem

---
+++ index.php
@@ -0,0 +1,752 @@
+<?php
+
+
+$bla = array(
+	'dev1' => array(
+		'type' => 'ir',
+		'label' => 'Beamer',
+		'rows' => array(
+			'00' => array(
+				'power' => array(
+					'icon' => 'fa-power-off',
+					'label' => 'Power'
+				),
+				'volup' => array(
+					'icon' => 'fa-chevron-up',
+					'label' => 'VOL+'
+				),
+				'voldown' => array(
+					'icon' => 'fa-chevron-down',
+					'label' => 'VOL-'
+				),
+			),
+			'01' => array(
+				'chup' => array(
+					'icon' => 'fa-chevron-up',
+					'label' => 'CHANNEL'
+				),
+				'chdown' => array(
+					'icon' => 'fa-chevron-down',
+					'label' => 'CHANNEL'
+				),
+			),
+			0 => array(
+				'src_hdmi1' => array(
+					'icon' => 'fa-sign-in',
+					'label' => 'HDMI1'
+				),
+				'src_hdmi2' => array(
+					'icon' => 'fa-sign-in',
+					'label' => 'HDMI2'
+				),
+				'src_hdmi3' => array(
+					'icon' => 'fa-sign-in',
+					'label' => 'HDMI3'
+				),
+			),
+			1 => array(
+				'emptyspace1' => NULL,
+				'up' => array(
+					'icon' => 'fa-caret-up',
+					'label' => ''
+				),
+				'emptyspace2' => NULL,
+			),
+			
+			2 => array(
+				'left' => array(
+					'icon' => 'fa-caret-left',
+					'label' => ''
+				),
+				'ok' => array(
+					'icon' => MULL,
+					'label' => 'OK'
+				),
+				'right' => array(
+					'icon' => 'fa-caret-right',
+					'label' => ''
+				),
+			),
+			
+			3 => array(
+				'emptyspace1' => NULL,
+				'down' => array(
+					'icon' => 'fa-caret-down',
+					'label' => NULL,
+				),
+				'emptyspace2' => NULL,
+			),
+			4 => array(
+				'num7' => array(
+					'label' => '7'
+				),
+				'num8' => array(
+					'label' => '8'
+				),
+				'num9' => array(
+					'label' => '9'
+				),
+				'slash' => array(
+					'label' => '/'
+				),
+			),
+			5 => array(
+				'num4' => array(
+					'label' => '4'
+				),
+				'num5' => array(
+					'label' => '5'
+				),
+				'num6' => array(
+					'label' => '6'
+				),
+				'raute' => array(
+					'label' => '#'
+				),
+			),
+			6 => array(
+				'num1' => array(
+					'label' => '1'
+				),
+				'num2' => array(
+					'label' => '2'
+				),
+				'num3' => array(
+					'label' => '3'
+				),
+				'point' => array(
+					'label' => '.'
+				),
+			),
+			
+			'pla' => array(
+				'stop' => array(
+					'icon' => 'fa-stop'
+				),
+				'play' => array(
+					'icon' => 'fa-play'
+				),
+				'pause' => array(
+					'icon' => 'fa-pause'
+				),
+				'rec' => array(
+					'icon' => 'fa-circle'
+				),
+			),
+			'plb' => array(
+				'rew' => array(
+					'icon' => 'fa-backward'
+				),
+				'ffw' => array(
+					'icon' => 'fa-forward'
+				),
+				'reletext' => array(
+					'icon' => 'fa-pause'
+				),
+				'topt' => array(
+					'label' => 'T.OPT'
+				),
+			),
+			'5er' => array(
+				'stop' => array(
+					'icon' => 'fa-stop'
+				),
+				'play' => array(
+					'icon' => 'fa-play'
+				),
+				'pause' => array(
+					'icon' => 'fa-pause'
+				),
+				'rec' => array(
+					'icon' => 'fa-circle'
+				),
+				'5tes' => array(
+					'icon' => 'fa-stop'
+				),
+			),
+			'6er' => array(
+				'stop' => array(
+					'icon' => 'fa-stop'
+				),
+				'play' => array(
+					'icon' => 'fa-play'
+				),
+				'pause' => array(
+					'icon' => 'fa-pause'
+				),
+				'rec' => array(
+					'icon' => 'fa-circle'
+				),
+				'5tes' => array(
+					'icon' => 'fa-stop'
+				),
+				'6tes' => array(
+					'icon' => 'fa-caret-down'
+				),
+			),
+		),
+	),
+	
+	
+	
+	'dev2' => array(
+		'type' => 'ir',
+		'label' => 'TV',
+		'rows' => array(
+			'00' => array(
+				'power' => array(
+					'icon' => 'fa-power-off',
+					'label' => 'Power'
+				),
+				'volup' => array(
+					'icon' => 'fa-chevron-up',
+					'label' => 'VOL+'
+				),
+				'voldown' => array(
+					'icon' => 'fa-chevron-down',
+					'label' => 'VOL-'
+				),
+			),
+			'01' => array(
+				'chup' => array(
+					'icon' => 'fa-chevron-up',
+					'label' => 'CHANNEL'
+				),
+				'chdown' => array(
+					'icon' => 'fa-chevron-down',
+					'label' => 'CHANNEL'
+				),
+			),
+			0 => array(
+				'src_hdmi1' => array(
+					'icon' => 'fa-sign-in',
+					'label' => 'HDMI1'
+				),
+				'src_hdmi2' => array(
+					'icon' => 'fa-sign-in',
+					'label' => 'HDMI2'
+				),
+				'src_hdmi3' => array(
+					'icon' => 'fa-sign-in',
+					'label' => 'HDMI3'
+				),
+			),
+			1 => array(
+				'emptyspace1' => NULL,
+				'up' => array(
+					'icon' => 'fa-caret-up',
+					'label' => ''
+				),
+				'emptyspace2' => NULL,
+			),
+			
+			2 => array(
+				'left' => array(
+					'icon' => 'fa-caret-left',
+					'label' => ''
+				),
+				'ok' => array(
+					'icon' => MULL,
+					'label' => 'OK'
+				),
+				'right' => array(
+					'icon' => 'fa-caret-right',
+					'label' => ''
+				),
+			),
+			
+			3 => array(
+				'emptyspace1' => NULL,
+				'down' => array(
+					'icon' => 'fa-caret-down',
+					'label' => NULL,
+				),
+				'emptyspace2' => NULL,
+			),
+			
+			'pla' => array(
+				'stop' => array(
+					'icon' => 'fa-stop'
+				),
+				'play' => array(
+					'icon' => 'fa-play'
+				),
+				'pause' => array(
+					'icon' => 'fa-pause'
+				),
+				'rec' => array(
+					'icon' => 'fa-circle'
+				),
+			),
+			'plb' => array(
+				'rew' => array(
+					'icon' => 'fa-backward'
+				),
+				'ffw' => array(
+					'icon' => 'fa-forward'
+				),
+				'reletext' => array(
+					'icon' => 'fa-pause'
+				),
+				'topt' => array(
+					'label' => 'T.OPT'
+				),
+			),
+			'5er' => array(
+				'stop' => array(
+					'icon' => 'fa-stop'
+				),
+				'play' => array(
+					'icon' => 'fa-play'
+				),
+				'pause' => array(
+					'icon' => 'fa-pause'
+				),
+				'rec' => array(
+					'icon' => 'fa-circle'
+				),
+				'5tes' => array(
+					'icon' => 'fa-stop'
+				),
+			),
+			'6er' => array(
+				'stop' => array(
+					'icon' => 'fa-stop'
+				),
+				'play' => array(
+					'icon' => 'fa-play'
+				),
+				'pause' => array(
+					'icon' => 'fa-pause'
+				),
+				'rec' => array(
+					'icon' => 'fa-circle'
+				),
+				'5tes' => array(
+					'icon' => 'fa-stop'
+				),
+				'6tes' => array(
+					'icon' => 'fa-caret-down'
+				),
+			),
+		),
+	),
+	
+	
+	'dev3' => array(
+		'type' => 'ir',
+		'label' => 'Verstärker',
+		'rows' => array(
+			'00' => array(
+				'power' => array(
+					'icon' => 'fa-power-off',
+					'label' => 'Power'
+				),
+				'volup' => array(
+					'icon' => 'fa-chevron-up',
+					'label' => 'VOL+'
+				),
+				'voldown' => array(
+					'icon' => 'fa-chevron-down',
+					'label' => 'VOL-'
+				),
+			),
+			'01' => array(
+				'chup' => array(
+					'icon' => 'fa-chevron-up',
+					'label' => 'CHANNEL'
+				),
+				'chdown' => array(
+					'icon' => 'fa-chevron-down',
+					'label' => 'CHANNEL'
+				),
+			),
+			0 => array(
+				'src_hdmi1' => array(
+					'icon' => 'fa-sign-in',
+					'label' => 'HDMI1'
+				),
+				'src_hdmi2' => array(
+					'icon' => 'fa-sign-in',
+					'label' => 'HDMI2'
+				),
+				'src_hdmi3' => array(
+					'icon' => 'fa-sign-in',
+					'label' => 'HDMI3'
+				),
+			),
+			1 => array(
+				'emptyspace1' => NULL,
+				'up' => array(
+					'icon' => 'fa-caret-up',
+					'label' => ''
+				),
+				'emptyspace2' => NULL,
+			),
+			
+			2 => array(
+				'left' => array(
+					'icon' => 'fa-caret-left',
+					'label' => ''
+				),
+				'ok' => array(
+					'icon' => MULL,
+					'label' => 'OK'
+				),
+				'right' => array(
+					'icon' => 'fa-caret-right',
+					'label' => ''
+				),
+			),
+			
+			3 => array(
+				'emptyspace1' => NULL,
+				'down' => array(
+					'icon' => 'fa-caret-down',
+					'label' => NULL,
+				),
+				'emptyspace2' => NULL,
+			),
+			
+			'pla' => array(
+				'stop' => array(
+					'icon' => 'fa-stop'
+				),
+				'play' => array(
+					'icon' => 'fa-play'
+				),
+				'pause' => array(
+					'icon' => 'fa-pause'
+				),
+				'rec' => array(
+					'icon' => 'fa-circle'
+				),
+			),
+			'plb' => array(
+				'rew' => array(
+					'icon' => 'fa-backward'
+				),
+				'ffw' => array(
+					'icon' => 'fa-forward'
+				),
+				'reletext' => array(
+					'icon' => 'fa-pause'
+				),
+				'topt' => array(
+					'label' => 'T.OPT'
+				),
+			),
+			'5er' => array(
+				'stop' => array(
+					'icon' => 'fa-stop'
+				),
+				'play' => array(
+					'icon' => 'fa-play'
+				),
+				'pause' => array(
+					'icon' => 'fa-pause'
+				),
+				'rec' => array(
+					'icon' => 'fa-circle'
+				),
+				'5tes' => array(
+					'icon' => 'fa-stop'
+				),
+			),
+			'6er' => array(
+				'stop' => array(
+					'icon' => 'fa-stop'
+				),
+				'play' => array(
+					'icon' => 'fa-play'
+				),
+				'pause' => array(
+					'icon' => 'fa-pause'
+				),
+				'rec' => array(
+					'icon' => 'fa-circle'
+				),
+				'5tes' => array(
+					'icon' => 'fa-stop'
+				),
+				'6tes' => array(
+					'icon' => 'fa-caret-down'
+				),
+			),
+		),
+	),
+	
+	
+	'dev4' => array(
+		'type' => 'ir',
+		'label' => 'BluRay',
+		'rows' => array(
+			'00' => array(
+				'power' => array(
+					'icon' => 'fa-power-off',
+					'label' => 'Power'
+				),
+				'volup' => array(
+					'icon' => 'fa-chevron-up',
+					'label' => 'VOL+'
+				),
+				'voldown' => array(
+					'icon' => 'fa-chevron-down',
+					'label' => 'VOL-'
+				),
+			),
+		),
+	),
+	
+	'dev5' => array(
+		'type' => 'ir',
+		'label' => 'Tuner',
+		'rows' => array(
+			'00' => array(
+				'power' => array(
+					'icon' => 'fa-power-off',
+					'label' => 'Power'
+				),
+				'volup' => array(
+					'icon' => 'fa-chevron-up',
+					'label' => 'VOL+'
+				),
+				'voldown' => array(
+					'icon' => 'fa-chevron-down',
+					'label' => 'VOL-'
+				),
+			),
+		),
+	),
+	
+	'dev6' => array(
+		'type' => 'ir',
+		'label' => 'CD',
+		'rows' => array(
+			'00' => array(
+				'power' => array(
+					'icon' => 'fa-power-off',
+					'label' => 'Power'
+				),
+				'volup' => array(
+					'icon' => 'fa-chevron-up',
+					'label' => 'VOL+'
+				),
+				'voldown' => array(
+					'icon' => 'fa-chevron-down',
+					'label' => 'VOL-'
+				),
+			),
+		),
+	),
+	
+	'dev7' => array(
+		'type' => 'ir',
+		'label' => 'Tape',
+		'rows' => array(
+			'00' => array(
+				'power' => array(
+					'icon' => 'fa-power-off',
+					'label' => 'Power'
+				),
+				'volup' => array(
+					'icon' => 'fa-chevron-up',
+					'label' => 'VOL+'
+				),
+				'voldown' => array(
+					'icon' => 'fa-chevron-down',
+					'label' => 'VOL-'
+				),
+			),
+		),
+	),
+	
+	'dev8' => array(
+		'type' => 'ir',
+		'label' => 'TV-Lift',
+		'rows' => array(
+			'00' => array(
+				'power' => array(
+					'icon' => 'fa-power-off',
+					'label' => 'Power'
+				),
+				'volup' => array(
+					'icon' => 'fa-chevron-up',
+					'label' => 'VOL+'
+				),
+				'voldown' => array(
+					'icon' => 'fa-chevron-down',
+					'label' => 'VOL-'
+				),
+			),
+		),
+	),
+	
+	'dev10' => array(
+		'type' => 'ir',
+		'label' => 'HDMI Streamer',
+		'rows' => array(
+			'00' => array(
+				'power' => array(
+					'icon' => 'fa-power-off',
+					'label' => 'Power'
+				),
+				'volup' => array(
+					'icon' => 'fa-chevron-up',
+					'label' => 'VOL+'
+				),
+				'voldown' => array(
+					'icon' => 'fa-chevron-down',
+					'label' => 'VOL-'
+				),
+			),
+		),
+	),
+	
+	
+);
+
+
+?><!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+
+    <title>HiFi Control</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/starter-template.css" rel="stylesheet">
+    
+    <!-- http://fortawesome.github.io/Font-Awesome/icons/#web-application -->
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    
+
+	<link href="vendor/bootstrap-notify/css/bootstrap-notify.css" rel="stylesheet">
+
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">HiFi</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+        <?php
+        foreach($bla as $device => $dConf) {
+        	echo '
+        		<li>
+        			<a class="scrollto" href="#s'.$device .'">' . $dConf['label'] . '</a>
+        			<a class="fire navpower" href="#"><i class="fa fa-power-off fa-lg"></i></a>
+        		</li>';
+        }
+        
+		
+		?>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <!--h1>Bootstrap starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p-->
+        
+        <?php
+        foreach($bla as $device => $dConf) {
+        	echo '<div class="section" id="s'. $device .'"><h3>' . $dConf['label'] . '</h3>' . "\n";
+			foreach($dConf['rows'] as $dRow) {
+				echo '<div class="brow clearfix brow' . count($dRow) . '">';
+				foreach($dRow as $cmd => $rowConfig) {
+					
+					if($rowConfig === NULL) {
+						$class = 'hideme';
+						$text = '';
+					} else {
+						$class = '';
+						$text = (($rowConfig['icon']) ? '<i class="fa '.$rowConfig['icon'].' fa-lg"></i>' : '') .
+							(($rowConfig['label']) ? ' ' . $rowConfig['label'] : ''); 
+					
+					}
+					echo '<button class="fire ' . $class . '">' . $text . "</button>\n";
+					
+					 
+				}
+				echo '</div>';
+			}
+			echo "</div>\n";
+        }
+        
+		
+		?>
+        
+        
+      </div>
+
+    </div><!-- /.container -->
+    
+    
+    <div class='notifications bottom-left'></div>
+    
+    
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-scrollTo/1.4.3/jquery.scrollTo.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="vendor/bootstrap-notify/js/bootstrap-notify.js"></script>
+    
+    <script type="text/javascript">
+    	$(document).ready(function(){
+    		$('.scrollto').on('click', function(){
+    			// TODO: hide nav only in mobile view
+    			$(".navbar-toggle").click();
+    			$.scrollTo($($(this).attr('href')).offset().top-50, 200);
+    		});
+    		$('.fire').on('click', function(){
+    			fireCmd('fire');
+    			return false;
+    		})
+    	});
+    	
+    	function fireCmd(param) {
+    		
+    		$('.bottom-left').notify({
+			    message: { text: 'TODO: send cmd' }
+			}).show();
+    		
+    	}
+    	
+    </script>
+  </body>
+</html>
